00218b5358e36368c02f05d6bbe540b6f4e4bbb0
DilonKaHackers <107928148+DilonKaHackers@users.noreply.github.com>
2024-12-25 07:24:39 +0530

    Update Collections.hpp
    corrected remove,multiple and add ,multiple

--- Collections.hpp
+++ Collections.hpp
@@ -29,37 +29,53 @@ public:
             cout << "DataHolder: " << id << " already exists in the collection: " << collection_name << endl;
         }
         else {
-            dataHolder.updateInCollectionVector(CN); // Update the original DataHolder
-            collectionMap.emplace(id, dataHolder);  // Insert by moving the updated DataHolder
+            dataHolder.addInCollectionVector(CN); 
+            collectionMap.emplace(id, dataHolder);  
             cout << "DataHolder: " << id << " added to the collection: " << collection_name << endl;
         }
     }
 
 
     // Add multiple DataHolders
-    void add_multiple_dataHolders(vector<DataHolder<x, y>>& dataHolders) {
-        string CN = this->collection_name;
+    void add_multiple_dataHolders(vector<DataHolder<x, y>*>& dataHolders) {
 
-        for (auto& dataHolder : dataHolders) {
-            add_to_collection(dataHolder);
+        for (auto* dataHolder : dataHolders) { 
+            if (dataHolder != nullptr) {       
+                add_to_collection(*dataHolder); 
+            }
+            else {
+                cout << "Null DataHolder encountered, skipping." << endl;
+            }
         }
     }
 
 
     // Remove a DataHolder by ID
-    void remove_from_collection(const string& id) {
-        if (collectionMap.erase(id)) {
+    void remove_from_collection(DataHolder<x, y>& dataHolder) {
+
+        string CN = this->collection_name;
+        string id = dataHolder.getID();
+
+        if (collectionMap.find(id) != collectionMap.end()) {
+            dataHolder.removeFromCollectionVector(CN); 
+            collectionMap.erase(id); 
             cout << "DataHolder: " << id << " removed from the collection: " << collection_name << endl;
         }
         else {
-            cout << "DataHolder: " << id << " does not exist in the collection: " << collection_name << endl;
+            cout << "DataHolder: " << id << " not found the collection: " << collection_name << endl;
         }
     }
 
     // Remove multiple DataHolders by IDs
-    void remove_multiple_dataHolders(const vector<string>& ids) {
-        for (const auto& id : ids) {
-            remove_from_collection(id);
+    void remove_multiple_dataHolders(vector<DataHolder<x, y>*>& dataHolders) {
+
+        for (auto* dataHolder : dataHolders) {
+            if (dataHolder != nullptr) {
+                remove_from_collection(*dataHolder);
+            }
+            else {
+                cout << "Null DataHolder encountered, skipping." << endl;
+            }
         }
     }
 
