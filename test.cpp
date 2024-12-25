32d83511dda86f84ffec1c22261b210d007f0d33
DilonKaHackers <107928148+DilonKaHackers@users.noreply.github.com>
2024-12-25 07:26:14 +0530

    Update test.cpp
    newer tests did at this time

--- test.cpp
+++ test.cpp
@@ -1,34 +1,35 @@
-#include <iostream>
-#include <vector>
-#include "DataHolder.hpp"
 #include "Collections.hpp"
 
 using namespace std;
 int main() {
-    // Create Collections
-    Collections<int, string> myCollection11("MyCollection11");
-    Collections<int, string> myCollection22("MyCollection22");
 
+    Collections<int, string> collection1("TestCollection1");
+    Collections<int, string> collection2("TestCollection2");
+    Collections<int, string> collection3("TestCollection3");
 
-    // Create DataHolder objects
     DataHolder<int, string> dh1("DH1");
     DataHolder<int, string> dh2("DH2");
     DataHolder<int, string> dh3("DH3");
 
-    // Add DataHolder objects to a vector
-    vector<DataHolder<int, string>> dataHolders = { dh1, dh2, dh3 };
-
-    // Add multiple DataHolders
-    myCollection11.add_to_collection(dh2);
-    myCollection11.add_to_collection(dh1);
-
-    myCollection22.add_to_collection(dh2);
+    vector<DataHolder<int, string>*> addDhVector = { &dh1, &dh2, &dh3 };
+    vector<DataHolder<int, string>*> removeDhVector = { &dh1, &dh2 };
 
 
-    // Verify updates
+    // Adding individually
+    collection1.add_to_collection(dh1);
+
+    collection2.add_multiple_dataHolders(addDhVector);
+    collection3.add_multiple_dataHolders(addDhVector);
+
+   
+    dh1.showCollectionNames();
     dh2.showCollectionNames();
+    dh3.showCollectionNames();
 
+    collection2.remove_multiple_dataHolders(removeDhVector);
 
+    dh1.showCollectionNames();
+    dh2.showCollectionNames();
 
     return 0;
 }
