7092609367dd7c6c597780cfcd4405784449efaf
DilonKaHackers <107928148+DilonKaHackers@users.noreply.github.com>
2024-12-25 13:16:51 +0530

    Update Collections.hpp
    added exists, rename, clear, getters

--- Collections.hpp
+++ Collections.hpp
@@ -14,11 +14,40 @@ private:
 public:
     // Constructors
     Collections(const string& collectionName) : collection_name(collectionName) {}
-    Collections() {
-        cout << "Enter the Collection name: ";
-        cin >> collection_name;
+    Collections() = default;
+
+    //getter
+    string getCollectionName(){
+        return this->collection_name;
+    }
+    unordered_map<string, DataHolder<x, y>> getCollectionMap(){
+        return this->collectionMap;
+    }
+    DataHolder<x, y> getDataHolder(string nameOfDataHolder) {
+        if (collectionMap.find(nameOfDataHolder) != collectionMap.end()) {
+            return collectionMap[nameOfDataHolder];
+        }
     }
 
+    //exists 
+    //Checks if dataholder exists with dataholder id
+    bool exists(string nameOfDataHolder) {
+        bool ans = false;
+        
+        if (collectionMap.find(nameOfDataHolder) != collectionMap.end()) {
+            ans = true;          
+        }
+        return ans;
+    }
+    //Checks if dataholder exists with dataholder parameter
+    bool exists(DataHolder<x,y> theDataHolder) {
+        bool ans = false;
+
+        if (collectionMap.find(theDataHolder.getID()) != collectionMap.end()) {
+            ans = true;
+        }
+        return ans;
+    }
     // Add a DataHolder by ID
     void add_to_collection(DataHolder<x, y>& dataHolder) {
 
@@ -29,8 +58,8 @@ public:
             cout << "DataHolder: " << id << " already exists in the collection: " << collection_name << endl;
         }
         else {
-            dataHolder.addInCollectionVector(CN); 
-            collectionMap.emplace(id, dataHolder);  
+            dataHolder.addInCollectionVector(CN);
+            collectionMap.emplace(id, dataHolder);
             cout << "DataHolder: " << id << " added to the collection: " << collection_name << endl;
         }
     }
@@ -39,9 +68,9 @@ public:
     // Add multiple DataHolders
     void add_multiple_dataHolders(vector<DataHolder<x, y>*>& dataHolders) {
 
-        for (auto* dataHolder : dataHolders) { 
-            if (dataHolder != nullptr) {       
-                add_to_collection(*dataHolder); 
+        for (auto* dataHolder : dataHolders) {
+            if (dataHolder != nullptr) {
+                add_to_collection(*dataHolder);
             }
             else {
                 cout << "Null DataHolder encountered, skipping." << endl;
@@ -57,8 +86,8 @@ public:
         string id = dataHolder.getID();
 
         if (collectionMap.find(id) != collectionMap.end()) {
-            dataHolder.removeFromCollectionVector(CN); 
-            collectionMap.erase(id); 
+            dataHolder.removeFromCollectionVector(CN);
+            collectionMap.erase(id);
             cout << "DataHolder: " << id << " removed from the collection: " << collection_name << endl;
         }
         else {
@@ -81,12 +110,33 @@ public:
 
     // Print the collection
     void printCollection() const {
+        if (this->collectionMap.empty()) {
+            cout << "Collection has no data";
+            return;
+        }
         cout << endl << "Collection name: " << collection_name << endl;
         for (const auto& pair : collectionMap) {
             cout << "ID: " << pair.first << endl;
             pair.second.printData();
         }
     }
+    // rename Collection
+    void renameCollection(const string& newCollectionName) {
+        this->collection_name = newCollectionName;
+    }
+
+    //clear collectionMap for a collection
+    void clearCollection() {
+        this->collectionMap.clear();
+    }
+
 };
 
 #endif // COLLECTIONS_HPP
+
+
+
+
+
+
+
